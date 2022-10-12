426972fab7787c3352461813fffb3ca7bd590bbb
Victor Litvynchuk <vendettich13@icloud.com>
2022-10-12 23:38:20 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { searchCountry } from './renders.js';
 import { refs } from './renders.js';
 
 const debounce = require('lodash.debounce');
-
 const DEBOUNCE_DELAY = 300;
 
 refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
